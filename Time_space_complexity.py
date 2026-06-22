4b682c785acdfe2336cf5ad11fe0fc17bfe3f33e
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-06-22 20:28:37 +0530

    Time & Space Complexity

---
+++ Time_space_complexity.py
@@ -0,0 +1,18 @@
+# Time Complexity: It tells how does run-time grow as input size grows?
+# --> It measures in Big - O notation
+# --> Constant time O(1)
+# --> Linear time(single loop) O(n)
+# --> Quadratic time(multi loop) O(n^2)
+# --> Logarithmic time O(log n) --- Each step cuts search space in half
+# --> O(n log n) used for merge and heap sort, better than O(n^2)
+
+# Space Complexity: how much extra memory is used?
+# --> O(1) fixed memory
+# --> O(n) extra array grows with input size
+
+# Complexity n -> 2n
+# O(1)       same
+# O(log n)   slightly big
+# O(n)       2x
+# O(n^2)     4x
+# O(n^3)     8x
